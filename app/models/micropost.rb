c0e285d9fae84458dd91d05b08d780b0bab8413b
Samuel Ulu <sam.e.ulu@gmail.com>
2011-04-10 08:47:33 -0400

    modified demo app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,8 @@
 class Micropost < ActiveRecord::Base
-  validates :content, :length => { maximum => 10 }
+  belongs_to :user
+  
+  
+  
+  
+  validates :content, :length => { :maximum => 140 }
 end
